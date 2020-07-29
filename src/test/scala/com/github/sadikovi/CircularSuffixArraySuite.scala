187ded7514a7a0b87a12c886a45e78862c700fa0
Ivan Sadikov <ivan.sadikov@gmail.com>
2020-07-29 22:28:35 +0200

    update test

--- src/test/scala/com/github/sadikovi/CircularSuffixArraySuite.scala
+++ src/test/scala/com/github/sadikovi/CircularSuffixArraySuite.scala
@@ -1,7 +1,7 @@
 package com.github.sadikovi
 
 class CircularSuffixArraySuite extends BenchmarkSuite {
-  test("test") {
+  test("small test") {
     // new CircularSuffixArray("ABCDAASDFWESDF")
     new CircularSuffixArray("ABRACADABRA!")
     // new CircularSuffixArray("19/02/20 09:33:25 WARN scheduler.TaskSetManager: Lost task 200.0 in stage 0.0 (TID xxx, t.y.z.com, executor 93): ExecutorLostFailure (executor 93 exited caused by one of the running tasks) Reason: Container killed by YARN for exceeding memory limits. 8.1 GB of 8 GB physical memory used. Consider boosting spark.yarn.executor.memoryOverhead.")
@@ -9,16 +9,24 @@ class CircularSuffixArraySuite extends BenchmarkSuite {
 
   // Mergesort:
   // Benchmark "benchmark":
-  // Min (ms): 158.667
-  // Med (ms): 160.322
-  // Max (ms): 209.8
+  // Min (ms): 337.371
+  // Med (ms): 342.588
+  // Max (ms): 391.029
 
   // 3-way quicksort:
   // Benchmark "benchmark":
-  // Min (ms): 81.366
-  // Med (ms): 82.195
-  // Max (ms): 99.966
-  private val input = "19/02/20 09:33:25 WARN scheduler.TaskSetManager: Lost task 200.0 in stage 0.0 (TID xxx, t.y.z.com, executor 93): ExecutorLostFailure (executor 93 exited caused by one of the running tasks) Reason: Container killed by YARN for exceeding memory limits. 8.1 GB of 8 GB physical memory used. Consider boosting spark.yarn.executor.memoryOverhead." * 10
+  // Min (ms): 173.279
+  // Med (ms): 174.528
+  // Max (ms): 192.642
+
+  // 5000 characters
+  private val input = (
+    "19/02/20 09:33:25 WARN scheduler.TaskSetManager: Lost task 200.0 in stage 0.0 (TID xxx, t.y.z.com, e" +
+    "xecutor 93): ExecutorLostFailure (executor 93 exited caused by one of the running tasks) Reason: Con" +
+    "tainer killed by YARN for exceeding memory limits. 8.1 GB of 8 GB physical memory used. Consider boo" +
+    "sting spark.yarn.executor.memoryOverhead. Caused by: org.apache.hadoop.fs.FileAlreadyExistsException" +
+    ": /user/hive/warehouse/tmp_supply_feb1/.spark-staging-blah-blah-blah/dt=2019-02-17/part-00200-blah-b"
+  ) * 10
 
   bench("benchmark", 100) {
     new CircularSuffixArray(input)
